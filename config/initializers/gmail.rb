bb4d5a54053ac0928b2dbcaaa05090f03bbcb762
Mark Chavez <los_populares@yahoo.com>
2013-04-01 15:45:23 +0800

    Configured email settings

--- config/initializers/gmail.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :domain               => 'your.host.name',
-  :user_name            => ENV('MAIL_USERNAME'), # full email address (user@your.host.name.com)
-  :password             => ENV('MAIL_PASSWORD'),
-  :authentication       => 'plain',
-  :enable_starttls_auto => true
-}
